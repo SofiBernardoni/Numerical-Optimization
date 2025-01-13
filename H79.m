227f533042fa9a7246932433356ff0ebd4f2fc79
gaiagiulio <s296267@studenti.polito.it>
2025-01-13 23:52:38 +0100

    J79,H79, truncated_newton, first test Rosenbrock-trucated, funzione visualiz

---
+++ H79.m
@@ -0,0 +1,12 @@
+function H=H79(x)
+% Function that computes the Hessian of function 79
+
+x_next=[x(2:end);0];
+x_prev=[0;x(1:end-1)];
+
+f=(3-x/10).*x+1-x_prev-2*x_next;
+diag0=5-f/5+(3-x/5).^2;
+diag_up= -2*(3-x/5)-(3-x_next/5);
+H=diag(diag0)+diag(diag_up,1);
+
+end
