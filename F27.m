3f6c34d2560b6a703f7f3f7f5e7745311399dfbe
SofiBernardoni <s296685@studenti.polito.it>
2025-01-13 19:17:06 +0100

    funzioni senza cicli

--- F27.m
+++ F27.m
@@ -1,7 +1,5 @@
-function F=F27(x,n)
-f_k=@(x,k) (x(k)-1)/sqrt(100000) ;
-F=0;
-for j=1:n
-    F=F+f_k(x,j)^2;
-end
-F=F+ (sum(x.^2)-0.25)^2;
+function F=F27(x)
+fk=(x-1)/sqrt(100000);
+F=sum(fk.^2);
+F=F+(sum(x.^2)-0.25)^2;
+F=F/2;
