caaabebd2e93967e83d38a446bae0b52a0861efb
SofiBernardoni <s296685@studenti.polito.it>
2025-01-13 18:42:32 +0100

    f16 e f27

--- F16.m
+++ F16.m
@@ -1,7 +1,7 @@
-function F=F16(n,x)
+function F=F16(x,n)
 F=0;
 x=[0;x; 0];
 for i = 2:n
-    F = F + (i * (  (1-cos(x(i)))  +sin(x(i-1))  - sin(x(i+1))    ) );
+    F = F + ((i-1) * (  (1-cos(x(i)))  +sin(x(i-1))  - sin(x(i+1))    ) );
 end 
 end
